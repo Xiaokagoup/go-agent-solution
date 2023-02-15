00aef91383ffae1e700c7adcef5d773a235cdada
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-02-15 10:13:00 +0100

    Update project structure

---
+++ src/runCommand/runCommand.go
@@ -0,0 +1,17 @@
+package runCommand
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("sh", "-c", "./script.sh")
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Printf("Error running script: %s\n", err)
+	} else {
+		fmt.Printf("Script output: %s\n", output)
+	}
+}
---
+++ src/runCommand/runCommand_test.go
@@ -0,0 +1,14 @@
+package runCommand
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	fmt.Println("Start test - main.go")
+
+	main()
+
+	fmt.Println("End test - main.go")
+}
