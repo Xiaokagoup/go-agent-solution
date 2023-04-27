58d9d7eef0cb03ede88e542266d4da42102d3e0c
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-27 15:44:32 +0200

    Create SendGETRequest

--- src/agent/agent.go
+++ src/agent/agent.go
@@ -1,7 +1,6 @@
 package agent
 
 import (
-	"bytes"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
@@ -9,7 +8,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"runtime"
 	"time"
 
 	agtHttp "github.com/JieanYang/HelloWorldGoAgent/src/agentHttp"
@@ -71,43 +69,117 @@ func GeneratePSK_key() string {
 	return psk
 }
 
+func GetOperationCommandFromBackend() {
+
+}
+
 func RunPeriodicTask() {
 	interval := 5 * time.Second
 	ticker := time.NewTicker(interval)
 
+	fmt.Println("RunPeriodicTask - start")
 	for range ticker.C {
-		SendPOSTRequest(BACKEND_ENDPOINT+"/node/api-docs", runtime.GOOS+" | I'm a request in RunPeriodicTask")
+		responseData, err := SendGETRequest(BACKEND_ENDPOINT + "/node/aws/getMockOperationCommand")
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Printf("%+v\n", responseData)
+		}
 	}
+	fmt.Println("RunPeriodicTask - end")
 }
 
-func SendPOSTRequest(url string, messages string) *http.Response {
-	type POSTData struct {
-		Messages string `json:"message"`
-	}
+type OperationResult struct {
+	ReturnCode int    `json:"returnCode"`
+	StdOut     string `json:"stdOut"`
+	StdErr     string `json:"stdErr"`
+}
+type OneOperationCommand struct {
+	Id               string          `json:"id"`
+	OperationCommand string          `json:"operationCommand"`
+	Status           string          `json:"status"`
+	OperationScript  string          `json:"operationScript"`
+	OperationResult  OperationResult `json:"operationResult"`
+	TryTimes         int             `json:"tryTimes"`
+}
+type ResponseData struct {
+	Result OneOperationCommand `json:"result"`
+}
 
-	postData := &POSTData{
-		Messages: messages,
-	}
-	jsonPostData, err := json.Marshal(postData)
-	if err != nil {
-		panic(err)
-	}
+func (rd ResponseData) String() string {
+	return fmt.Sprintf("ID: %s\nOperation Command: %s\nStatus: %s\nOperation Script: %s\nReturn Code: %d\nStdOut: %s\nStdErr: %s\nTry Times: %d",
+		rd.Result.Id,
+		rd.Result.OperationCommand,
+		rd.Result.Status,
+		rd.Result.OperationScript,
+		rd.Result.OperationResult.ReturnCode,
+		rd.Result.OperationResult.StdOut,
+		rd.Result.OperationResult.StdErr,
+		rd.Result.TryTimes)
+}
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPostData))
+func SendGETRequest(url string) (*ResponseData, error) {
+	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	return resp
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var responseData ResponseData
+	err = json.Unmarshal(body, &responseData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &responseData, nil
 }
+
+// func SendPOSTRequest(url string, messages string) ResponseData {
+// 	type POSTData struct {
+// 		Messages string `json:"message"`
+// 	}
+
+// 	postData := &POSTData{
+// 		Messages: messages,
+// 	}
+// 	jsonPostData, err := json.Marshal(postData)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	// req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPostData))
+// 	req, err := http.NewRequest("GET", url, bytes.NewBuffer(jsonPostData))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	req.Header.Set("Content-Type", "application/json")
+
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	fmt.Println("Response body:", resp)
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	var data ResponseData
+// 	err = json.Unmarshal(body, &data)
+// 	if err != nil {
+// 		// handle error
+// 	}
+
+// 	return data
+// }
 
 func (agent *Agent) Init() {
 	// load config
@@ -120,19 +192,4 @@ func (agent *Agent) Init() {
 
 	psk := GeneratePSK_key()
 	agentMetadataManager.GetOrCreateConfigFileWithSpecifiedPskKey(psk) // save psk key to config file
-
-	resp := SendPOSTRequest(BACKEND_ENDPOINT+"/node/api-docs", runtime.GOOS+" | Hello World")
-
-	// Check response status code
-	if resp.StatusCode != http.StatusOK {
-		panic(fmt.Sprintf("unexpected status code: %d", resp.StatusCode))
-	}
-
-	// Print response body
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	bodyString := string(bodyBytes)
-	fmt.Println("Response body:", bodyString)
 }
