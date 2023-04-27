31fb2ea75350834fc2fff4afa810805653b0ff29
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-27 15:48:45 +0200

    Create src/tools/requestWithBackend/requestWithBackend.go

--- src/agent/agent.go
+++ src/agent/agent.go
@@ -3,15 +3,13 @@ package agent
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"time"
 
 	agtHttp "github.com/JieanYang/HelloWorldGoAgent/src/agentHttp"
 	"github.com/JieanYang/HelloWorldGoAgent/src/tools/agentMetadataManager"
+	"github.com/JieanYang/HelloWorldGoAgent/src/tools/requestWithBackend"
 )
 
 const (
@@ -20,8 +18,6 @@ const (
 )
 
 var WrongStateError = errors.New("Can't take the operation in the current state")
-
-var BACKEND_ENDPOINT string = "https://ff66-2a01-cb16-60-e0c3-a023-d11-c4af-ce6a.ngrok-free.app"
 
 type Agent struct {
 	state int
@@ -69,74 +65,15 @@ func GeneratePSK_key() string {
 	return psk
 }
 
-func GetOperationCommandFromBackend() {
-
-}
-
 func RunPeriodicTask() {
 	interval := 5 * time.Second
 	ticker := time.NewTicker(interval)
 
 	fmt.Println("RunPeriodicTask - start")
 	for range ticker.C {
-		responseData, err := SendGETRequest(BACKEND_ENDPOINT + "/node/aws/getMockOperationCommand")
-		if err != nil {
-			fmt.Println("Error:", err)
-		} else {
-			fmt.Printf("%+v\n", responseData)
-		}
+		requestWithBackend.GetOperationCommandFromBackend()
 	}
 	fmt.Println("RunPeriodicTask - end")
-}
-
-type OperationResult struct {
-	ReturnCode int    `json:"returnCode"`
-	StdOut     string `json:"stdOut"`
-	StdErr     string `json:"stdErr"`
-}
-type OneOperationCommand struct {
-	Id               string          `json:"id"`
-	OperationCommand string          `json:"operationCommand"`
-	Status           string          `json:"status"`
-	OperationScript  string          `json:"operationScript"`
-	OperationResult  OperationResult `json:"operationResult"`
-	TryTimes         int             `json:"tryTimes"`
-}
-type ResponseData struct {
-	Result OneOperationCommand `json:"result"`
-}
-
-func (rd ResponseData) String() string {
-	return fmt.Sprintf("ID: %s\nOperation Command: %s\nStatus: %s\nOperation Script: %s\nReturn Code: %d\nStdOut: %s\nStdErr: %s\nTry Times: %d",
-		rd.Result.Id,
-		rd.Result.OperationCommand,
-		rd.Result.Status,
-		rd.Result.OperationScript,
-		rd.Result.OperationResult.ReturnCode,
-		rd.Result.OperationResult.StdOut,
-		rd.Result.OperationResult.StdErr,
-		rd.Result.TryTimes)
-}
-
-func SendGETRequest(url string) (*ResponseData, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var responseData ResponseData
-	err = json.Unmarshal(body, &responseData)
-	if err != nil {
-		return nil, err
-	}
-
-	return &responseData, nil
 }
 
 // func SendPOSTRequest(url string, messages string) ResponseData {
---
+++ src/tools/requestWithBackend/requestWithBackend.go
@@ -0,0 +1,69 @@
+package requestWithBackend
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var BACKEND_ENDPOINT string = "https://ff66-2a01-cb16-60-e0c3-a023-d11-c4af-ce6a.ngrok-free.app"
+
+func GetOperationCommandFromBackend() {
+	responseData, err := SendGETRequest(BACKEND_ENDPOINT + "/node/aws/getMockOperationCommand")
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Printf("%+v\n", responseData)
+	}
+}
+
+type OperationResult struct {
+	ReturnCode int    `json:"returnCode"`
+	StdOut     string `json:"stdOut"`
+	StdErr     string `json:"stdErr"`
+}
+type OneOperationCommand struct {
+	Id               string          `json:"id"`
+	OperationCommand string          `json:"operationCommand"`
+	Status           string          `json:"status"`
+	OperationScript  string          `json:"operationScript"`
+	OperationResult  OperationResult `json:"operationResult"`
+	TryTimes         int             `json:"tryTimes"`
+}
+type ResponseData struct {
+	Result OneOperationCommand `json:"result"`
+}
+
+func (rd ResponseData) String() string {
+	return fmt.Sprintf("===============\nID: %s\nOperation Command: %s\nStatus: %s\nOperation Script: %s\nReturn Code: %d\nStdOut: %s\nStdErr: %s\nTry Times: %d\n===============\n",
+		rd.Result.Id,
+		rd.Result.OperationCommand,
+		rd.Result.Status,
+		rd.Result.OperationScript,
+		rd.Result.OperationResult.ReturnCode,
+		rd.Result.OperationResult.StdOut,
+		rd.Result.OperationResult.StdErr,
+		rd.Result.TryTimes)
+}
+
+func SendGETRequest(url string) (*ResponseData, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var responseData ResponseData
+	err = json.Unmarshal(body, &responseData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &responseData, nil
+}
