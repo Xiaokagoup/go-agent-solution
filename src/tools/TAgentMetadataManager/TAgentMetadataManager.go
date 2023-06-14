1c67d6af435fffe82e958d7817f1e86dc85dc900
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-06-14 10:47:47 +0200

    Add backend readMetadataFromFile and writeMetadataToFile

--- src/tools/TAgentMetadataManager/TAgentMetadataManager.go
+++ src/tools/TAgentMetadataManager/TAgentMetadataManager.go
@@ -5,13 +5,69 @@
 package TAgentMetadataManager
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
 
 	"github.com/spf13/viper"
 )
+
+func main() {
+	// Test function
+	fileName := "metadata.json"
+
+	metadatas := Metadata{
+		ClientId:      "3",
+		CloudProvider: "AWS",
+		Region:        "eu-west-3",
+		NodeType:      "t2.micro",
+		CreatedAt:     "2023-02-16T11:22:35.040Z",
+		PSK_Key:       "12345678901234567890123456789012",
+	}
+
+	writeMetadataToFile(fileName, metadatas)
+
+	fileContent, _ := readMetadataFromFile(fileName)
+	fmt.Printf("fileContent %T, %v \n", fileContent, fileContent)
+	fmt.Println("fileContent", *fileContent)
+
+}
+
+func readMetadataFromFile(fileName string) (*Metadata, error) {
+	// Read the contents of the file
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the JSON data in to a slice of User structs
+	var metadata *Metadata
+	err = json.Unmarshal(data, &metadata)
+	if err != nil {
+		return nil, err
+	}
+
+	return metadata, nil
+}
+
+func writeMetadataToFile(fileName string, metadata Metadata) error {
+	// Convert the slice of Usser structs to JSON data
+	data, err := json.Marshal(metadata)
+	if err != nil {
+		return err
+	}
+
+	// Write the JSON data to the file
+	err = ioutil.WriteFile(fileName, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 type Metadata struct {
 	ClientId      string `json:"clientId"`
